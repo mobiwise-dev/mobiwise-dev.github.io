7699fa7150d32416915c72cd1e2778c38d7e7b87
MobiWise <96530435+mobiwise-codes@users.noreply.github.com>
2023-04-27 14:03:39 +0200

    Deploy website - based on 877cb982547f1d296448976938c2f0171d7ebd71

--- assets/js/814f3328.8213b24e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmobiwise_dev_github_io=self.webpackChunkmobiwise_dev_github_io||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome to this blog dedicated to Mobirise","permalink":"/blog/welcome-on-this-blog"}]}')}}]);
---
+++ assets/js/814f3328.feb2f051.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmobiwise_dev_github_io=self.webpackChunkmobiwise_dev_github_io||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome on this Mobirise blog","permalink":"/blog/welcome-on-Mobirise-blog"}]}')}}]);
