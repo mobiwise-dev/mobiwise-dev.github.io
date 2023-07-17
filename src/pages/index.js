d438c36308c7c963e402ee1d7a73481a0b341d54
MobiWise <hi.mobiwise@gmail.com>
2023-07-17 13:01:54 +0200

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,19 +42,13 @@ function HomepageHeader() {
           }}
         />
         <div className="buttons" style={{ marginTop: "7vh" }}>
-          <Link
-            className="button button--outline button--secondary button--lg"
-            to={"extensions"}
-          >
+          <Link className="button button--outline button--secondary button--lg" to={"extensions"}>
             MobiWise Extensions
           </Link>
         </div>
       </header>
 
-      <section
-        className="container"
-        style={{ paddingTop: "10vh", paddingBottom: "10vh" }}
-      >
+      <section className="container" style={{ paddingTop: "10vh", paddingBottom: "10vh" }}>
         <h1 style={{ textAlign: "center" }}>About {siteConfig.title}</h1>
         <p style={{ textAlign: "center" }}>
           MobiWise brings together several projects and know-how around{" "}
@@ -68,62 +62,26 @@ function HomepageHeader() {
         </p>
       </section>
 
-      <section
-        className="container"
-        style={{ paddingTop: "10vh", paddingBottom: "10vh" }}
-      >
+      <section className="container" style={{ paddingTop: "10vh", paddingBottom: "10vh" }}>
         <h1 style={{ textAlign: "center" }}>What is Mobirise ?</h1>
         <p style={{ textAlign: "center" }}>
-          Mobirise is a <b>free</b> offline app <b>for Window and Mac</b> to
-          easily create small/medium websites, landing pages, online resumes and
-          portfolios, promo sites for apps, events, services and products.
+          Mobirise is a <b>free</b> offline app <b>for Window and Mac</b> to easily create small/medium websites, landing pages, online resumes and portfolios, promo sites for apps, events, services and products.
         </p>
       </section>
 
-      <section
-        className="container"
-        style={{ paddingTop: "10vh", paddingBottom: "10vh" }}
-      >
-        <h1 style={{ textAlign: "center" }}>
-          Do you work with Mobirise team ?
-        </h1>
+      <section className="container" style={{ paddingTop: "10vh", paddingBottom: "10vh" }}>
+        <h1 style={{ textAlign: "center" }}>Do you work with Mobirise team ?</h1>
         <p style={{ textAlign: "center" }}>
-          It is important to note all <b>MobiWise projects</b> are independent
-          and is not affiliated with Mobirise in any way. The creator, MobiWise,
-          is not affiliated with the Mobirise team and is simply an enthusiastic
-          user of the platform who wants to help the community by providing a
-          central location for Mobirise resources.
+          It is important to note all <b>MobiWise projects</b> are independent and is not affiliated with Mobirise in any way. The creator, MobiWise, is not affiliated with the Mobirise team and is simply an enthusiastic user of the platform who wants to help the community by providing a central location for Mobirise resources.
         </p>
       </section>
 
-      <section
-        className="container"
-        style={{ paddingTop: "10vh", paddingBottom: "10vh" }}
-      >
+      <section className="container" style={{ paddingTop: "10vh", paddingBottom: "10vh" }}>
         <h1 style={{ textAlign: "center" }}>Why make a donation ?</h1>
-        <p style={{ textAlign: "center" }}>
-          I have created this projects and website for Mobirise to help you
-          create even more powerful and attractive websites. My goal is to
-          provide you with quality tools to enhance your experience with
-          Mobirise.{" "}
-        </p>
-        <p style={{ textAlign: "center" }}>
-          I am working on this projects in my spare time, out of my passion for
-          creating websites and developing innovative solutions.{" "}
-        </p>
-        <p style={{ textAlign: "center" }}>
-          However, maintaining, hosting, domain name,... comes at a cost. If you
-          like the blocks and extensions I propose, and if you want to support
-          my work and contribute to the sustainability of this project, I invite
-          you to make a donation. Each contribution, even modest, is precious
-          and will allow me to continue to develop new features, to correct bugs
-          and to ensure compatibility with future versions of Mobirise.{" "}
-        </p>
-        <p style={{ textAlign: "center" }}>
-          I sincerely thank you for your support and I hope you will continue to
-          enjoy the blocks and extensions I provide for Mobirise. If you have
-          any questions or suggestions, please feel free to contact me.
-        </p>
+        <p style={{ textAlign: "center" }}>I have created this projects and website for Mobirise to help you create even more powerful and attractive websites. My goal is to provide you with quality tools to enhance your experience with Mobirise. </p>
+        <p style={{ textAlign: "center" }}>I am working on this projects in my spare time, out of my passion for creating websites and developing innovative solutions. </p>
+        <p style={{ textAlign: "center" }}>However, maintaining, hosting, domain name,... comes at a cost. If you like the blocks and extensions I propose, and if you want to support my work and contribute to the sustainability of this project, I invite you to make a donation. Each contribution, even modest, is precious and will allow me to continue to develop new features, to correct bugs and to ensure compatibility with future versions of Mobirise. </p>
+        <p style={{ textAlign: "center" }}>I sincerely thank you for your support and I hope you will continue to enjoy the blocks and extensions I provide for Mobirise. If you have any questions or suggestions, please feel free to contact me.</p>
       </section>
     </>
   );
@@ -132,10 +90,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       {/* <main>
         <HomepageFeatures />
